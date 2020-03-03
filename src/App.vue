7df8a2cb5894740228fe719cd4243df65f12eeda
Imrul Khan <imrul69@gmail.com>
2020-03-03 21:25:16 +0600

    Todo list show with check and uncheck.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,59 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">    
+    <Todos v-bind:todos="todos"/>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script> 
+import Todos from './components/Todos';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Todos
+  }, 
+  data(){
+    return {
+      todos:[
+          {
+            id: 1,
+            title: 'Todo One',
+            completed: false
+          },
+          {
+            id: 2,
+            title: 'Todo Two',
+            completed: true
+          },
+          {
+            id: 3,
+            title: 'Todo Three',
+            completed: false
+          },
+          {
+            id: 4,
+            title: 'Todo Four',
+            completed: true
+          },
+          {
+            id: 5,
+            title: 'Todo Five',
+            completed: false
+          }
+        ]
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body{
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
 }
 </style>
